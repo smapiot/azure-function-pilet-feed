14bef5a2e15fc87a1f011b85ef87de389eb6f810
Waqas Ali <arien.online@yahoo.com>
2020-10-11 19:57:23 +0200

    add environment variable

--- FeedService/Models/PackageJson.cs
+++ FeedService/Models/PackageJson.cs
@@ -1,52 +1,50 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Text.Json.Serialization;
 
 namespace FeedService.Models
 {
-    public class PackageJson
-    {
-        /// <summary>
-        /// Represents the pilet name
-        /// </summary>
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-        public string Description { get; set; }
-        [JsonPropertyName("version")]
-        public string Version { get; set; }
-        public bool Preview { get; set; }
-        public string Custom { get; set; }
-        [JsonPropertyName("authorinfo")]
-        public Author AuthorInfo { get; set; }
-        public string Main { get; set; }
-        public string Lincense { get; set; }
-        public DevDependencies devDependencies { get; set; }
-        public Dependencies dependencies { get; set; }
-        public PeerDependencies peerDependencies { get; set; }
-    }
+  public class PackageJson
+  {
+    /// <summary>
+    /// Represents the pilet name
+    /// </summary>
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    public string Description { get; set; }
+    [JsonPropertyName("version")]
+    public string Version { get; set; }
+    public bool Preview { get; set; }
+    public string Custom { get; set; }
+    [JsonPropertyName("authorinfo")]
+    public Author AuthorInfo { get; set; }
+    public string Main { get; set; }
+    [JsonPropertyName("license")]
+    public string License { get; set; }
+    [JsonPropertyName("devDependencies")]
+    public DevDependencies DevDependencies { get; set; }
+    [JsonPropertyName("dependencies")]
+    public IDictionary<string, string> Dependencies { get; set; }
+    [JsonPropertyName("peerDependencies")]
+    public PeerDependencies PeerDependencies { get; set; }
+  }
 
-    public class Author
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-        [JsonPropertyName("email")]
-        public string Email { get; set; }
-    }
+  public class Author
+  {
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+  }
 
-    public class DevDependencies
-    {
-        [JsonPropertyName("names")]
-        public List<string> Names { get; set; }
-    }
+  public class DevDependencies
+  {
+    [JsonPropertyName("names")]
+    public List<string> Names { get; set; }
+  }
 
-    public class Dependencies
-    {
-
-    }
-    public class PeerDependencies
-    {
-    }
+  public class PeerDependencies
+  {
+  }
 }
--- FeedService/Services/PiletService.cs
+++ FeedService/Services/PiletService.cs
@@ -1,173 +1,173 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ICSharpCode.SharpZipLib.GZip;
-using ICSharpCode.SharpZipLib.Tar;
-using System.IO;
-using System.IO.Compression;
-using System.Text.Json;
 using FeedService.Infrastructure;
 using FeedService.Models;
+using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace FeedService.Services
 {
-    public class PiletService : IPiletService
+  public class PiletService : IPiletService
+  {
+    private readonly IStorageAccountRepository _storageAccountRepository;
+    public PiletService(IStorageAccountRepository storageAccountRepository)
     {
-        private readonly IStorageAccountRepository _storageAccountRepository;
-        public PiletService(IStorageAccountRepository storageAccountRepository)
-        {
-            _storageAccountRepository = storageAccountRepository;
+      _storageAccountRepository = storageAccountRepository;
 
-        }
-
-       /// <summary>
-       /// Gets the latest pilets from storage account.
-       /// </summary>
-       /// <returns></returns>
-        public async Task<PiletApiResponse> GetLatestPilets()
-        {
-            var latestPiletsInfo = await _storageAccountRepository.GetLatestPiletsInfoAsync();
-            var piletResponse = new PiletApiResponse();
-
-            foreach (var pilet in latestPiletsInfo)
-            {
-                var version = ExtractVersion(pilet);
-                var name = ExtractName(pilet);
-                /// This service runs locally. If you want to publish it
-                //you need to handle change this URL accordingly.
-                var url = "http://localhost:7071/api/files/" + name + "/" + version + "/index.js";
-
-                piletResponse.Items.Add(new PiletMetaData
-                {
-                    Name = name,
-                    Link = url,
-                    Version = version
-
-                });
-            }
-            return piletResponse;
-        }
-
-        /// <summary>
-        /// Returns individual files requested by Piral instance.
-        /// </summary>
-        /// <param name="fileName"></param>
-        /// <param name="version"></param>
-        /// <param name="piletName"></param>
-        /// <returns>File content.</returns>
-        public async Task<string> GetPiletFile(string fileName, string version, string piletName)
-        {
-            return await _storageAccountRepository.GetPiletFile(fileName, version, piletName);
-        }
-
-        /// <summary>
-        /// Publishes the Pilet to a storage account.
-        /// </summary>
-        /// <returns></returns>
-
-        public Task<bool> PublishPilet(byte[] fileData)
-        {
-            try
-            {
-                var packageJson = new PackageJson();
-                var packageFiles = new List<PackageFile>();
-                Stream inStream = new MemoryStream(fileData);
-                Stream gzipStream = new GZipInputStream(inStream);
-                using (var tarInputStream = new TarInputStream(gzipStream, Encoding.UTF8))
-                {
-                    TarEntry entry;
-                    while ((entry = tarInputStream.GetNextEntry()) != null)
-                    {
-                        using (var fileContents = new MemoryStream())
-                        {
-                            tarInputStream.CopyEntryContents(fileContents);
-                            var content = Encoding.UTF8.GetString(fileContents.GetBuffer());
-                            var fileName = GetFileName(entry.Name);
-                            var package = new PackageFile
-                            {
-                                Content = content
-                            };
-                            switch (fileName)
-                            {
-                                case "index.js.map":
-                                    package.Path = entry.Name;
-                                    package.FileName = fileName;
-                                    break;
-                                case "main.css":
-                                    package.Path = entry.Name;
-                                    package.FileName = fileName;
-                                    break;
-                                case "index.js":
-                                    package.Path = entry.Name;
-                                    package.FileName = fileName;
-                                    break;
-                                case "package.json":
-                                    package.Path = entry.Name;
-                                    package.FileName = fileName;
-                                    packageJson = JsonSerializer.Deserialize<PackageJson>(content);
-                                    break;
-                                default:
-                                    package.Path = entry.Name;
-                                    package.FileName = fileName;
-                                    break;
-                            }
-                            packageFiles.Add(package);
-                        }
-                    }
-                }
-
-                var zippedFiles = CreateZipFile(packageJson, packageFiles);
-                _storageAccountRepository.UploadPilet(packageFiles, packageJson, zippedFiles.ToArray());
-            }
-            catch (Exception)
-            {
-                return Task.FromResult(false);
-            }
-            return Task.FromResult(false);
-        }
-
-        private MemoryStream CreateZipFile(PackageJson packageJson, IEnumerable<PackageFile> files)
-        {
-            try
-            {
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    using (ZipArchive archive = new ZipArchive(ms, ZipArchiveMode.Create, true))
-                    {
-                        foreach (var file in files)
-                        {
-                            ZipArchiveEntry entry = archive.CreateEntry(file.Path);
-                            using (StreamWriter writer = new StreamWriter(entry.Open()))
-                            {
-                                writer.Write(file.Content);
-                            }
-                        }
-                    }
-
-                    return ms;
-                }
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        private static string GetFileName(string path)
-        {
-            var s = path.Split('/');
-            return s[s.Length - 1];
-        }
-
-        private string ExtractName(string pilet)
-        {
-            return pilet.Split('/')[0];
-        }
-
-        private string ExtractVersion(string pilet)
-        {
-            return pilet.Split('/')[1].Substring(0, 5);
-        }
     }
+
+    /// <summary>
+    /// Gets the latest pilets from storage account.
+    /// </summary>
+    /// <returns></returns>
+    public async Task<PiletApiResponse> GetLatestPilets()
+    {
+      var latestPiletsInfo = await _storageAccountRepository.GetLatestPiletsInfoAsync();
+      var piletResponse = new PiletApiResponse();
+
+      foreach (var pilet in latestPiletsInfo)
+      {
+        var version = ExtractVersion(pilet);
+        var name = ExtractName(pilet);
+        /// This service runs locally. If you want to publish it
+        //you need to handle change this URL accordingly.
+        var url = Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME") + "/api/files/" + name + "/" + version + "/index.js";
+
+        piletResponse.Items.Add(new PiletMetaData
+        {
+          Name = name,
+          Link = url,
+          Version = version
+
+        });
+      }
+      return piletResponse;
+    }
+
+    /// <summary>
+    /// Returns individual files requested by Piral instance.
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <param name="version"></param>
+    /// <param name="piletName"></param>
+    /// <returns>File content.</returns>
+    public async Task<string> GetPiletFile(string fileName, string version, string piletName)
+    {
+      return await _storageAccountRepository.GetPiletFile(fileName, version, piletName);
+    }
+
+    /// <summary>
+    /// Publishes the Pilet to a storage account.
+    /// </summary>
+    /// <returns></returns>
+
+    public Task<bool> PublishPilet(byte[] fileData)
+    {
+      try
+      {
+        var packageJson = new PackageJson();
+        var packageFiles = new List<PackageFile>();
+        Stream inStream = new MemoryStream(fileData);
+        Stream gzipStream = new GZipInputStream(inStream);
+        using (var tarInputStream = new TarInputStream(gzipStream, Encoding.UTF8))
+        {
+          TarEntry entry;
+          while ((entry = tarInputStream.GetNextEntry()) != null)
+          {
+            using (var fileContents = new MemoryStream())
+            {
+              tarInputStream.CopyEntryContents(fileContents);
+              var content = Encoding.UTF8.GetString(fileContents.GetBuffer());
+              var fileName = GetFileName(entry.Name);
+              var package = new PackageFile
+              {
+                Content = content
+              };
+              switch (fileName)
+              {
+                case "index.js.map":
+                  package.Path = entry.Name;
+                  package.FileName = fileName;
+                  break;
+                case "main.css":
+                  package.Path = entry.Name;
+                  package.FileName = fileName;
+                  break;
+                case "index.js":
+                  package.Path = entry.Name;
+                  package.FileName = fileName;
+                  break;
+                case "package.json":
+                  package.Path = entry.Name;
+                  package.FileName = fileName;
+                  packageJson = JsonSerializer.Deserialize<PackageJson>(content);
+                  break;
+                default:
+                  package.Path = entry.Name;
+                  package.FileName = fileName;
+                  break;
+              }
+              packageFiles.Add(package);
+            }
+          }
+        }
+
+        var zippedFiles = CreateZipFile(packageJson, packageFiles);
+        _storageAccountRepository.UploadPilet(packageFiles, packageJson, zippedFiles.ToArray());
+      }
+      catch (Exception)
+      {
+        return Task.FromResult(false);
+      }
+      return Task.FromResult(false);
+    }
+
+    private MemoryStream CreateZipFile(PackageJson packageJson, IEnumerable<PackageFile> files)
+    {
+      try
+      {
+        using (MemoryStream ms = new MemoryStream())
+        {
+          using (ZipArchive archive = new ZipArchive(ms, ZipArchiveMode.Create, true))
+          {
+            foreach (var file in files)
+            {
+              ZipArchiveEntry entry = archive.CreateEntry(file.Path);
+              using (StreamWriter writer = new StreamWriter(entry.Open()))
+              {
+                writer.Write(file.Content);
+              }
+            }
+          }
+
+          return ms;
+        }
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+
+    private static string GetFileName(string path)
+    {
+      var s = path.Split('/');
+      return s[s.Length - 1];
+    }
+
+    private string ExtractName(string pilet)
+    {
+      return pilet.Split('/')[0];
+    }
+
+    private string ExtractVersion(string pilet)
+    {
+      return pilet.Split('/')[1].Substring(0, 5);
+    }
+  }
 }
